0631dca49608ae679e9792ca7c37577515a9f1ac
Gaonkar30 <akashggaonkar@gmail.com>
2024-06-20 12:13:36 +0530

    started with products page

--- admin/pages/products.js
+++ admin/pages/products.js
@@ -1,7 +1,8 @@
 import Layout from "@/components/Layout";
+import Link from "next/link";
 
-export default function Products(){
-    return(
-        <Layout>Products page </Layout>
-    );
+export default function Products() {
+  return <Layout>
+    <Link  className="bg-gray-300 text-black rounded-md py-1 px-2" href={'/products/new'}>Add new Products</Link>
+  </Layout>;
 }
