15bb4afc0bd33a8bcbdc51b4c8ea2325a3bcac11
Gaonkar30 <akashggaonkar@gmail.com>
2024-06-20 11:42:27 +0530

    admin dashboard styles

--- admin/pages/index.js
+++ admin/pages/index.js
@@ -1,21 +1,12 @@
-
-import { Inter } from 'next/font/google'
-import { useSession, signIn, signOut } from "next-auth/react"
-const inter = Inter({ subsets: ['latin'] })
+import { Inter } from "next/font/google";
+import { useSession, signIn, signOut } from "next-auth/react";
+import Nav from "@/components/nav"
+import Layout from "@/components/Layout";
 
 export default function Home() {
-  const { data: session } = useSession();
-  if(!session){
-    return (
-      <div className="bg-blue-900 w-screen h-screen flex items-center">
-        <div className='text-center w-full'>
-          <button onClick={()=>signIn('google')} className='bg-white p-2 px-4 rounded-lg'> Login with Google</button>
-        </div>
-      </div>
-    )
-  }
   return (
-    <div className='bg-blue-900 w-screen h-screen'>logged in {session.user.email}</div>
+    <Layout>
+      test
+    </Layout>
   )
-  
 }
